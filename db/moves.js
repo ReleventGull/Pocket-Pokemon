2ecf6834b085ab003140bda1fdf0e4db53c6cac1
Jaron <jaronchretien@yahoo.com>
2022-12-12 23:50:33 -0600

    feat(src/Game): Grabbed Possible moves for pokemon (Not applied)

--- db/moves.js
+++ db/moves.js
@@ -13,6 +13,26 @@ const moves = [{
     type : 'Normal',
     power: 85,
 },
+{
+    name: 'Water Gun',
+    type : 'Water',
+    power: 40,
+},
+{
+    name: 'Peck',
+    type : 'Flying',
+    power: 35,
+},
+{
+    name: 'Razor Leaf',
+    type : 'Grass',
+    power: 55,
+},
+{
+    name: 'Dragon Rage',
+    type : 'Dragon',
+    power: 100,
+},
 ]
 
 module.exports = moves
