bc79d2dda037e837f392d5b2fce1b21f0b9baa41
Jaron <jaronchretien@yahoo.com>
2022-12-21 21:07:32 -0600

    Added Starter Pokemon and Progress Bar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import {fetchAllPokemon, fetchAllMoves, fetchPokemonById } from "./apiCalls/index"
+import {fetchAllPokemon, fetchAllMoves, fetchPokemonById, fetchPokemonRates} from "./apiCalls/index"
 import Game from './Game';
 
 const App = () => {
     const [isLoaded, setIsLoaded] = useState(false);
-   
     const [pokemon, setPokemon] = useState([]);
     const [pokemonMoves, setPokemonMoves] = useState([])
     const [starters, setStarters] = useState([])
+    const [loadBar, setLoadBar] = useState(0)
 
   
 
@@ -21,7 +21,9 @@ const App = () => {
     useEffect(() => {
       let pokeObject = []
     const getAllPokemon = async () => {
-          for (let i = 1; i <= 10; i++) {
+          for (let i = 1; i <= 100; i++) {
+            setLoadBar(i)
+            let pokemonStats = await fetchPokemonRates(i)
             let pokeMon = await fetchPokemonById(i)
             if (pokeMon.types[1]) {
               pokeObject.push({
@@ -29,14 +31,20 @@ const App = () => {
                 type1: pokeMon.types[0].type.name,
                 type2: pokeMon.types[1].type.name,
                 base_experience: pokeMon.base_experience,
-                stats: pokeMon.stats
+                stats: pokeMon.stats,
+                catch_rate: pokemonStats.capture_rate,
+                legendary: pokemonStats.is_legendary,
+                mythical: pokemonStats.is_mythical,
                  })
             }else {
               pokeObject.push({
                 name: pokeMon.name,
                 type1: pokeMon.types[0].type.name,
                 base_experience: pokeMon.base_experience,
-                stats: pokeMon.stats
+                stats: pokeMon.stats,
+                catch_rate: pokemonStats.capture_rate,
+                legendary: pokemonStats.is_legendary,
+                mythical: pokemonStats.is_mythical,
                  })
             }
           }
@@ -58,8 +66,10 @@ const App = () => {
     isLoaded && starters ? 
   
     <Game  starters={starters} pokemonMoves={pokemonMoves} pokemon={pokemon}/>:
-    'loading'
-  
+    <div className='loadingBar'>
+      <h1 >Loading...</h1>
+    <progress value={loadBar} max='100'></progress>
+    </div>
 
     
     
