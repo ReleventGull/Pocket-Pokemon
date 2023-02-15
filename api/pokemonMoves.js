71f1a9be34ce972fccc52d99b77f05b1c5c260b7
Jaron <jaronchretien@yahoo.com>
2023-02-15 14:30:22 -0600

    feat(Adds user front end and converts majority of pokemon data to backend

--- api/pokemonMoves.js
+++ api/pokemonMoves.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const movesRouter = express()
-const {getAllmoves} = require('../db/moves.js')
+const {createMove} = require('../db/moves.js')
 
 movesRouter.get('/', async(req, res, send) => {
     try {
@@ -8,6 +8,28 @@ movesRouter.get('/', async(req, res, send) => {
     res.send(moves)
     }catch(error) {
         next(error)
+    }
+})
+
+movesRouter.post('/', async(req, res, next) => {
+    try {
+        const {name, type, category, pp, power, accuracy, learnedBy} = req.body
+    
+        for(let i = 0; i < learnedBy.length; i++) {
+            const createdMove = await createMove({
+                name: name,
+                type: type,
+                category: category,
+                pp: pp,
+                power: power,
+                accuracy: accuracy,
+                learnedBy: learnedBy[i].name
+            })
+        }
+        res.send({message:"Completed!"})
+    }catch(error) {
+        console.error("There was an error creating the moves in the api", error)
+        throw error
     }
 })
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import e from "cors";
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import {fetchAllPokemon, fetchAllMoves} from "./apiCalls/index"
-import { uploadLevels, uploadingPokemon, fetchPokemonById, fetchPokemonRates, fetchPokemonLevels,getAllLevels } from "./apiCalls/seedApi";
+import { uploadLevels, uploadingPokemon, fetchPokemonById, fetchPokemonRates, fetchPokemonLevels, fetchMoves, seedMoves } from "./apiCalls/seedApi";
 import Game from './Game';
+
+
+
+
 
 const App = () => {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -11,11 +15,12 @@ const App = () => {
     const [pokemonMoves, setPokemonMoves] = useState([])
     const [starters, setStarters] = useState([])
     const [loadBar, setLoadBar] = useState(0)
-    console.log(pokemon)
     //For seeding all the pokemon, acts as a toggle. If pokemon is present in the database, set to false, if not , set to true
-    const [seedData, setSeedData] = useState(true)
+    const [seedData, setSeedData] = useState(false)
     //
     let numberofPokemon = 10
+    
+    
     const seedPokemon = async() => {
       for(let i=1; i <= numberofPokemon; i++)  {
         let pokemonStats = await fetchPokemonRates(i)
@@ -45,22 +50,41 @@ const App = () => {
             })
         }
       }
-      fetchGameData()
     }
+    const fetchSeedMoves = async() => {
+      for(let b = 1; b <= 60; b++) {
+        const result = await fetchMoves(b)
+        console.log(result.accuracy, b)
+        seedMoves({
+          name: result.name,
+          type: result.type.name,
+          category: result.damage_class.name,
+          pp: result.pp,
+          power: result.power,
+          accuracy: result.accuracy,
+          learnedBy: result.learned_by_pokemon
+  
+      })
+    }
+    return
+  }
     const seedPokeData = async() => {
       for(let i = 1; i <= 6; i++){
         const result = await fetchPokemonLevels(i)
-        await uploadLevels({name: result.name, levels: result.levels})
+         await uploadLevels({name: result.name, levels: result.levels})
       }
-        await seedPokemon()
+      
+      await seedPokemon()
+      await fetchSeedMoves()
+      await fetchGameData()
     }
-
     const fetchGameData = async() => {
       let allPokemon = await fetchAllPokemon()
       setPokemon(allPokemon)
       setIsLoaded(true)
       console.log(allPokemon)
     }
+    
     useEffect(() => {
       if(seedData) {
         seedPokeData()
