afee150bce3c4d8c75dbd2cd9e767d2fa30857a4
Jaron <jaronchretien@yahoo.com>
2023-03-25 14:13:34 -0500

    feat(styling): Sets up rendering for the categories on the bag page

--- src/Bag.js
+++ src/Bag.js
@@ -6,7 +6,15 @@ const Bag = ({setDisplay}) => {
         <div className="bagContainer">
             <div className="topBag">
                 <div className="bagCategories">
-
+                    <div className="categoryContainer">
+                        <div>All</div>
+                        <div>Pokeballs</div>
+                        <div>Healing</div>
+                        <div>Revival</div>
+                        <div>PP</div>
+                        <div>Status</div>
+                        <div>Stat</div>
+                    </div>
                 </div>
                 <button onClick={() => setDisplay('')}className="exitShop">X</button>
             </div>
