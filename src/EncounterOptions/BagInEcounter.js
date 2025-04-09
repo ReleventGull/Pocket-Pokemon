0a50a25dad2196a345f2742d78abf352c090282a
Jaron <jaronchretien@yahoo.com>
2025-04-09 11:56:43 -0500

    feat(switchAfterFailedCapture): Switches to enemy pokemon after failed captire

--- src/EncounterOptions/BagInEcounter.js
+++ src/EncounterOptions/BagInEcounter.js
@@ -5,10 +5,10 @@ const UseButton = ({useItem}) => {
         <button onClick={() => useItem()} className='useEncounterButton'>Use</button>
     )
 }
-const BagInEncounter = ({setEncounter, setMessage, pokemonEncountered, token, setView, animateBall}) => {
+const BagInEncounter = ({setPlayerTurn, setEncounter, setMessage, pokemonEncountered, token, setView, animateBall}) => {
     return (
         <div className="bagEncounterBody">
-            <Bag  setEncounter={setEncounter} setMessage={setMessage} animateBall={animateBall} pokemonEncountered={pokemonEncountered} UseButton={UseButton} setView={setView} token={token}/>
+            <Bag  setPlayerTurn={setPlayerTurn} setEncounter={setEncounter} setMessage={setMessage} animateBall={animateBall} pokemonEncountered={pokemonEncountered} UseButton={UseButton} setView={setView} token={token}/>
             
         </div>
     )
