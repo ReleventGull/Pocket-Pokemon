d98c5cb057b26b0f83188ff7231c1a977f731639
Jaron Chretien <109637509+ReleventGull@users.noreply.github.com>
2025-08-15 15:19:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,32 +6,27 @@ const morgan = require('morgan')
 const client = require('./db/index')
 const path = require('path');
 
-//Connect to the database
+
 
 client.connect()
 
-//Define Express Function
+
 const app = express()
 
-//Define our PORT
+
 const {PORT = 4000} = process.env
-
-
-//For Logging
-
+app.use(morgan('dev'))
+app.use(express.json())
 app.use(cors())
+
+
+app.use('/api', apiRouter)
+
 app.use(express.static(require('path').join(__dirname, 'build')));
 app.get('*', (req, res ,next) => {
 res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.use(morgan('dev'))
-app.use(express.json())
 
-//Use all our routes
-
-app.use('/api', apiRouter, (req,res,next) => {
-    res.send("Welcome to the Pokemon API! Here is where we store all the pokemon data so that you can use algorithms in order to develop the game. Enjoy!")
-})
 
 //Show that our port is current up and running
 app.listen(PORT, () => {
